706f235ff1a76f495e4cc1d38ed1c609418257f2
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-04-04 14:34:55 -0400

    added webpack, chance and babel

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var Chance = require("chance");
+
 console.log("in javascript");
+
 console.log(chance.bool());
+
+var chance = new Chance();
+
+var my_random_string = chance.string();
+console.log(my_random_string);
+
+// console.log("HELLOOOOOOO");
